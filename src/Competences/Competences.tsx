9d32b4dd8fdbc3f3d461c4f2462fc9b03b97f16d
Mig <goujonmael@gmail.com>
2025-01-04 14:46:20 +0100

    Update Competences component styles and enhance header with social links and CV download

--- src/Competences/Competences.tsx
+++ src/Competences/Competences.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { competences } from './competences';
 import './Competences.css';
+import { Link } from 'react-router-dom';
 
 export default function Competences() {
     return (
@@ -8,9 +9,23 @@ export default function Competences() {
             <h1 className="title">
                 <span className="title-first-letter">C</span>ompétences
             </h1>
-            <p className="description">
-                Voici les compétences acquises au cours de ma formation en informatique dans la spécialité réseau et sécurité.
-            </p>
+            <div className="description">
+                <p>
+                    Acquises lors de ma formation en informatique <Link className="underline" to='https://www.univ-tlse3.fr/decouvrir-nos-diplomes/but-informatique-parcours-deploiement-dapplications-communicantes-et-securisees-toulouse' target="_blank">spécialité réseau et sécurité</Link>.
+                </p>
+                <p>
+                    Le niveau 1 est acquis en première année, le niveau 2 en deuxième année etc.
+                </p>
+                <p>
+                    Les niveaux sont communs à toutes les spécialités (développement / bases de données / réseau et sécurité).
+                </p>
+                <p>
+                    C'est pourquoi le niveau 3 n'est atteint que pour les compétences en réseau et sécurité.
+                </p>
+                <p>
+                    <Link className="underline" to='https://www.univ-tlse3.fr/decouvrir-nos-diplomes/but-informatique-parcours-deploiement-dapplications-communicantes-et-securisees-toulouse#programme' target="_blank">Programme national de formation</Link>
+                </p>
+            </div>
             <div className="competences">
                 {competences.map((competence) => (
                     <div key={competence.id} className="competence">
--- src/Header/Header.tsx
+++ src/Header/Header.tsx
@@ -15,8 +15,79 @@ export default function Header() {
         <a href="/competences" className="nav-a">Compétences</a>
         <a href="/projets-univ" className="nav-a">Projets universitaires</a>
         <a href="/projets-perso" className="nav-a">Projets personnels</a>
+        <div className="nav-footer">
+          <a
+            href="https://www.linkedin.com/in/ma%C3%ABl-goujon-88635b227"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              color="white"
+              aria-hidden
+              src="/images/linkedin.svg"
+              alt="LinkedIn icon"
+              width={30}
+              height={30}
+            />
+            LinkedIn
+          </a>
+          <a
+            href="https://github.com/maelgoujon"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              aria-hidden
+              src="/images/git.svg"
+              alt="GitHub icon"
+              width={30}
+              height={30}
+            />
+            Git Etudiant
+          </a>
+          <a
+            href="https://github.com/goujonmael"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              aria-hidden
+              src="/images/git.svg"
+              alt="GitHub icon"
+              width={30}
+              height={30}
+            />
+            Git Personnel
+          </a>
+          <a
+            /* télécharger le CV */
+            href="/files/CV_GOUJON_Mael.pdf"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              aria-hidden
+              src="/images/cv.svg"
+              alt="CV icon"
+              width={30}
+              height={30}
+            />
+            Mon CV
+          </a>
+          {/* prendre contact */}
+          <a href="mailto:goujonmael@gmail.com">
+            <img
+              aria-hidden
+              src="/images/mail.svg"
+              alt="Mail icon"
+              width={30}
+              height={30}
+            />
+            Contact
+          </a>
+        </div>
       </div>
-      <button className="burger-menu" onClick={toggleMenu}>
+      <button className={`burger-menu ${isOpen ? 'open' : ''}`} onClick={toggleMenu}>
         <span className="burger-bar"></span>
         <span className="burger-bar"></span>
         <span className="burger-bar"></span>
